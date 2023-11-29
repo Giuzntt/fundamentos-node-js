65e5d201bd42073c6c94a0b1d16061603392416b
Giulianno Zanetti <giuzntt@gmail.com>
2023-11-29 13:29:47 -0300

    Add route for deleting user

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,13 @@
 import { Database } from "./database.js";
 import { randomUUID } from "crypto";
+import { buildRoutePath } from "./utils/build-route-path.js";
 
 const database = new Database();
 
 export const routes = [
   {
     method: "GET",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: async (req, res) => {
       const users = await database.select("users");
       res.setHeader("Content-Type", "application/json");
@@ -16,7 +17,7 @@ export const routes = [
   },
   {
     method: "POST",
-    path: "/users",
+    path: buildRoutePath("/users"),
     handler: async (req, res) => {
       res.writeHead(201, { "Content-Type": "application/json" });
       const { name, email } = req.body;
@@ -31,6 +32,18 @@ export const routes = [
       database.insert("users", user);
 
       return res.end("Criação de Usuários");
+    },
+  },
+  {
+    method: "DELETE",
+    path: buildRoutePath("/user/:id"),
+    handler: async (req, res) => {
+      res.writeHead(200, { "Content-Type": "application/json" });
+      const { id } = req.body;
+
+      database.delete("users", id);
+
+      return res.end("Usuário deletado");
     },
   },
 ];
